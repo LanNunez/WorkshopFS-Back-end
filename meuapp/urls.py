c9fb95d26344b0503f82858ddd63ef6bb9d57984
Lan Nunez <arllanextreme@gmail.com>
2026-03-19 16:26:27 -0300

    CRUD NO DJANGO
    FINALIZANDO O CRUD

--- meuapp/urls.py
+++ meuapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import listar_usuarios, criar_usuario, atualizar_usuario, deletar_usuario
 
 urlpatterns = [
-    path('index/', views.home, name='home'),
+    path('listar/', listar_usuarios, name='listar_usuarios'),
+    path('criar/', criar_usuario, name='criar_usuario'),
+    path('editar/<int:pk>/', atualizar_usuario, name= 'atualizar_usuario'),
+    path('deletar/<int:pk>/', deletar_usuario, name= 'deletar_usuario'),
 ]
--- meuapp/views.py
+++ meuapp/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Usuario
+from .form import UsuarioForm
 
-def home(requests):
-    return render(requests,"home.html")
 
+def criar_usuario(request):
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_usuarios')
+    else:
+        form = UsuarioForm()
+    return render(request, 'form.html', {'form': form})
+
+
+def listar_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'list.html', {'usuarios': usuarios})
+
+def atualizar_usuario(request, pk):
+    usuario = Usuario.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST, instance=usuario)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_usuarios')
+    else:
+        form = UsuarioForm(instance=usuario)
+    return render(request, 'form.html', {'form': form})
+
+def deletar_usuario(request, pk):
+    usuario = Usuario.objects.get(pk=pk)
+    if request.method == 'POST':
+        usuario.delete()
+        return redirect('listar_usuarios')
+    return render(request, 'confirmar_delete.html', {'usuario': usuario})
 # Create your views here.
